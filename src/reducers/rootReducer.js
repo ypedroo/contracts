147e1ac5fd8d09b73e192cefef122114f6e55b9e
ypedro <ynoa.pedro@outlook.com>
2019-10-01 23:06:08 -0300

    add rootReducer

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,12 @@
+import { root } from "postcss";
+
+const initialState = {
+    parts: [],
+    contracts: [],
+}
+
+const rootReducer = (state = initialState, action) => {
+    return state;
+}
+
+export default rootReducer;
