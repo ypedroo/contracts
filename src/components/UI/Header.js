0947bee8ac3ba14663ad364f2a5e5a2b658aa997
ypedro <ynoa.pedro@outlook.com>
2019-10-02 23:23:55 -0300

    Add assets folder directly on the project

--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import logo from '../../assets/logo.png';
 
 // import { Container } from './styles';
 
 export default class Header extends Component {
     render() {
         return <div >
-            <img src="../../../public/logo192.png" alt="logo"></img>
+            <img src={logo} alt="logo"></img>
             <h1> Contraktor </h1>
         </div>;
     }
