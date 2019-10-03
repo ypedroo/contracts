2b576482f77a3438b57974acab87dfd8fe7cab6d
ypedro <ynoa.pedro@outlook.com>
2019-10-02 23:44:02 -0300

    Fix: missing router embracing component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from './components/Home';
+import PartForm from './components/Part/PartForm';
 
 function App() {
   return (
     <div className="App">
+      <Router>
         <Route exact path="/" component={Home} />
-        {/* <Route path="/parts" component={PartList} />
-        <Route path="/contracts" component={ContractList} /> */}
+        <Route path="/parts" component={PartForm} />
+        {/* <Route path="/contracts" component={ContractList} /> */}
+      </Router>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import PartForm from '../components/Part/PartForm'
+import { Link } from 'react-router-dom';
+// import PartForm from '../components/Part/PartForm'
 import Header from '../components/UI/Header'
 
 // import { Container } from './styles';
@@ -9,7 +10,7 @@ export default class components extends Component {
         return <div>
             <Header />
             <button
-            onClick={<PartForm />}>
+            onClick={<Link to="/parts" />}>
                 Register new part 
             </button>
             
