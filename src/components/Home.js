f751709c80c4d07010c3945dce278fc6824a1133
ypedro <ynoa.pedro@outlook.com>
2019-10-02 23:18:33 -0300

    Adds header

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import PartForm from '../components/Part/PartForm'
+import Header from '../components/UI/Header'
 
 // import { Container } from './styles';
 
 export default class components extends Component {
     render() {
         return <div>
+            <Header />
             <PartForm />
         </div> ;
     }
---
+++ src/components/UI/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+// import { Container } from './styles';
+
+export default class Header extends Component {
+    render() {
+        return <div >
+            <img src="../../../public/logo192.png" alt="logo"></img>
+            <h1> Contraktor </h1>
+        </div>;
+    }
+}
