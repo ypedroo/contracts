1d2d2dfb58e0c696b66b78643ee44fa74bece690
ypedro <ynoa.pedro@outlook.com>
2019-10-02 23:37:42 -0300

    Add router to appJS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+        <Route exact path="/" component={Home} />
+        {/* <Route path="/parts" component={PartList} />
+        <Route path="/contracts" component={ContractList} /> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/rootReducer';
 
+
 const store = createStore(rootReducer);
 
 ReactDOM.render(<Provider store={store}> <App /> </Provider>, document.getElementById('root'));
