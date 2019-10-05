e1013e9fb3e47c0fc4299467f8fcc665c79c3e35
ynoa <ynoa.pedro@outlook.com>
2019-10-05 13:40:07 -0300

    Adding changes to gitignore

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,26 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 import Header from '../components/UI/Header'
-
-// import { Container } from './styles';
 
 export default class components extends Component {
     render() {
         return <div>
             <Header />
             <Link to="/parts" >
-                <button type="button">
-                    Register new part
-            </button>
+                <Button variant="outline-primary">
+                    Register part
+                </Button>
             </Link>
             <Link to="/contracts" >
-                <button type="button">
-                    Register new contracts
-            </button>
+                <Button
+                    node="a"
+                    waves="light"
+                    large
+                    style={{ marginRight: '5px' }}
+                >
+                    Register contracts
+            </Button>
             </Link>
         </div>;
     }
