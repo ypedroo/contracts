ac55ea76d7c8162c9934741ee550e1cb90afe559
ynoa <ynoa.pedro@outlook.com>
2019-10-05 13:47:07 -0300

    Fix: React Bootstrap

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap-theme.min.css';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
