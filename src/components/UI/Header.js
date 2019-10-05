b7569df05fbce187319be7c381454516669c9881
ynoa <ynoa.pedro@outlook.com>
2019-10-05 13:58:25 -0300

    addin redux form snippet

--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -1,19 +1,11 @@
 import React, { Component } from 'react';
 import logo from '../../assets/logo.png';
-import dev from '../../assets/ypedro.jpg';
-
 
 export default class Header extends Component {
     render() {
         return <div >
             <img src={logo} alt="logo"></img>
             <h1> Contraktor </h1>
-            <a href="https://ynoapedro.com">
-                <img 
-                src={dev} 
-                alt="resposibleDev"
-                style={ { maxWidth:"50px"} }></img>
-            </a>
         </div>;
     }
 }
