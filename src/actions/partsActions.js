eb40ddd32eb3715785fb3ac0573c76bd6107050f
ynoa <ynoa.pedro@outlook.com>
2019-10-06 01:14:29 -0300

    Adding axios

--- src/actions/partsActions.js
+++ src/actions/partsActions.js
@@ -1,4 +1,4 @@
-import {ADD_PART} from './actionTypes'
+import { ADD_PART } from './actionTypes'
 
 export const submitRegister = content => ({
     type: ADD_PART,
--- src/sagas/rootSaga.js
+++ src/sagas/rootSaga.js
@@ -1,3 +1,26 @@
-export function* rootSaga() {
-    yield console.log('Hello Sagas!')
+import { call, put, takeEvery } from 'redux-saga/effects';
+import { ADD_PART, SUCCESS_ADD_PART, FAIL_ADD_PART } from '../actions/actionTypes';
+import Api from '...'
+
+
+function* postPart(action) {
+    try {
+        const user = axios({
+            method: 'post',
+            url: baseUrl + 'applications/' + appName + '/dataexport/plantypes' + plan,
+            headers: {}, 
+            data: {
+              foo: 'bar', // This is the body part
+            }
+          });
+        yield put({ type: SUCCESS_ADD_PART, user: user });
+    } catch (e) {
+        yield put({ type: FAIL_ADD_PART, message: e.message });
+    }
 }
+
+function* rootSaga() {
+    yield takeEvery(ADD_PART, postPart);
+}
+
+export default rootSaga;
