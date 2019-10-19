4b48c62d2aaf898770cbbbff4464ff8d2b71b18a
ynoa <ynoa.pedro@outlook.com>
2019-10-19 18:24:25 -0300

    Fix clean form after submit

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,6 +1,21 @@
 import { combineReducers } from "redux";
 import partsReducer from "./partsReducer";
 import { reducer as formReducer } from 'redux-form';
+import { SUCCESS_ADD_PART } from '../actions/actionTypes'
 
+const reducers = {
+    partsReducer,
+    form: formReducer.plugin({
+        partForm: (state, action) => {
+            switch (action.type) {
+                case SUCCESS_ADD_PART:
+                    return undefined;
+                default:
+                    return state;
+            }
+        }
+    })
+}
+const reducer = combineReducers(reducers);
 
-export default combineReducers({ partsReducer, form: formReducer });
+export default reducer;
