a780eab35e7b8bbf55ec389f1bf4e418d732fcfe
ynoa <ynoa.pedro@outlook.com>
2019-10-20 20:22:42 -0300

    Style header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 import logo from '../assets/logo.png';
+import styled from 'styled-components';
+
+const Container = styled.div`
+postion: absolute;
+width:100%;
+height: 100%;
+display: inline-block;
+margin-botton: 70px;
+`
+
+const Img = styled.img`
+margin-botton: 70px;
+width: 70px;
+`
+
 
 export default class Header extends Component {
     render() {
-        return <div >
-            <img className="headerLogo" src={ logo } alt="logo"></img>
-        </div>;
+        return (
+            <Container>
+                <div >
+                    <Img className="headerLogo" src={logo} alt="logo"></Img>
+                </div>
+            </Container>
+        );
     }
 }
