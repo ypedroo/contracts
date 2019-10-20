f840bc5176c469d1f56e36e093cc2e95d4d469c9
ynoa <ynoa.pedro@outlook.com>
2019-10-20 20:08:37 -0300

    Adding container styles

--- src/pages/PartForm.js
+++ src/pages/PartForm.js
@@ -4,6 +4,12 @@ import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import { submitForm } from '../store/actions/partsActions';
 import Header from '../components/Header';
+import styled from 'styled-components';
+
+const Container = styled.div`
+margin: 8px;
+border: 1px solid;
+`
 
 class PartForm extends Component {
 
@@ -13,90 +19,93 @@ class PartForm extends Component {
     render() {
         const { handleSubmit, pristine, reset, submitting } = this.props;
         return (
-            <form onSubmit={handleSubmit(this.handleSubmit)}>
-                <div>
-                    <Header />
-                </div>
-                <div className="PartForm">
-                    <label>First Name</label>
+            <Container>
+                <form onSubmit={handleSubmit(this.handleSubmit)}>
                     <div>
-                        <Field
-                            name="name"
-                            component="input"
-                            type="text"
-                            placeholder="First Name"
-                        />
+                        <Header />
                     </div>
-                </div>
-                <div>
-                    <label>Last Name</label>
+                    <div className="PartForm">
+                        <label>First Name</label>
+                        <div>
+                            <Field
+                                name="name"
+                                component="input"
+                                type="text"
+                                placeholder="First Name"
+                            />
+                        </div>
+                    </div>
                     <div>
-                        <Field
-                            name="lastName"
-                            component="input"
-                            type="text"
-                            placeholder="Last Name"
-                        />
+                        <label>Last Name</label>
+                        <div>
+                            <Field
+                                name="lastName"
+                                component="input"
+                                type="text"
+                                placeholder="Last Name"
+                            />
+                        </div>
                     </div>
-                </div>
-                <div>
-                    <label>Email</label>
                     <div>
-                        <Field
-                            name="email"
-                            component="input"
-                            type="text"
-                            placeholder="Email"
-                        />
+                        <label>Email</label>
+                        <div>
+                            <Field
+                                name="email"
+                                component="input"
+                                type="text"
+                                placeholder="Email"
+                            />
+                        </div>
                     </div>
-                </div>
-                <div>
-                    <label>Cpf</label>
                     <div>
-                        <Field
-                            name="cpf"
-                            component="input"
-                            type="number"
-                            placeholder="CPF"
-                        />
+                        <label>Cpf</label>
+                        <div>
+                            <Field
+                                name="cpf"
+                                component="input"
+                                type="number"
+                                placeholder="CPF"
+                            />
+                        </div>
                     </div>
-                </div>
-                <div>
-                    <label>Phone</label>
                     <div>
-                        <Field
-                            name="phone"
-                            component="input"
-                            type="number"
-                            placeholder="Phone"
-                        />
+                        <label>Phone</label>
+                        <div>
+                            <Field
+                                name="phone"
+                                component="input"
+                                type="number"
+                                placeholder="Phone"
+                            />
+                        </div>
                     </div>
-                </div>
 
-                <div>
-                    <button type="submit" disabled={pristine || submitting}>
-                        Submit
+                    <div>
+                        <button type="submit" disabled={pristine || submitting}>
+                            Submit
             </button>
-                    <button type="button" disabled={pristine || submitting} onClick={reset}>
-                        Clear Values
+                        <button type="button" disabled={pristine || submitting} onClick={reset}>
+                            Clear Values
             </button>
-                </div>
-            </form>
+                    </div>
+                </form>
+            </Container>
+
         )
     }
 
 }
 
 const mapDispatchToProps = dispatch =>
-    bindActionCreators({submitForm}, dispatch);
+    bindActionCreators({ submitForm }, dispatch);
 
-    
-    PartForm =  reduxForm({
-        form: 'partForm',
-    })(PartForm);
-    
+
+PartForm = reduxForm({
+    form: 'partForm',
+})(PartForm);
+
 export default PartForm = connect(
-        null,
-        mapDispatchToProps
-    )(PartForm);
+    null,
+    mapDispatchToProps
+)(PartForm);
 
