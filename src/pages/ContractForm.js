5fed7f53e4c8db0864d92897f0dbac5a63cf8639
ynoa <ynoa.pedro@outlook.com>
2019-11-05 23:27:33 -0300

    Add dropzone to upload contracts

--- src/pages/ContractForm.js
+++ src/pages/ContractForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Container } from '../styles/styles';
+import Dropzone from 'react-dropzone'
 import Header from '../components/Header';
 
 export default class ContractForm extends Component {
@@ -9,8 +10,18 @@ export default class ContractForm extends Component {
                 <Header />
                 <div className="ContractForm" alt="ContractForm">
                     <h1> Contracts </h1>
+                    <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+                        {({ getRootProps, getInputProps }) => (
+                            <section>
+                                <div {...getRootProps()}>
+                                    <input {...getInputProps()} />
+                                    <p>drop the contract here, or click to select it</p>
+                                </div>
+                            </section>
+                        )}
+                    </Dropzone>
                 </div>
-                
+
             </Container>
 
         );
