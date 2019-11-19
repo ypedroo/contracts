50de2789a1d13298d39360d00082d24143e04322
ypedro <ynoa.pedro@outlook.com>
2019-11-19 12:52:56 -0300

    Create function to rerceive the file

--- src/pages/ContractForm.js
+++ src/pages/ContractForm.js
@@ -4,13 +4,14 @@ import Dropzone from 'react-dropzone'
 import Header from '../components/Header';
 
 export default class ContractForm extends Component {
+    getContract = value => console.log('got it', value);
     render() {
         return (
             <Container>
                 <Header />
                 <div className="ContractForm" alt="ContractForm">
                     <h1> Contracts </h1>
-                    <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+                    <Dropzone className="ContractDropZone"onDrop={acceptedFiles => this.getContract(acceptedFiles)}>
                         {({ getRootProps, getInputProps }) => (
                             <section>
                                 <div {...getRootProps()}>
