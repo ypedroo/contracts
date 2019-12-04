02493c05448dfd331041b10653c0b890b33d698f
ynoa <ynoa.pedro@outlook.com>
2019-12-04 00:52:14 -0300

    Adding dispatcher to contracts form

--- src/pages/ContractForm.js
+++ src/pages/ContractForm.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import { Container } from '../styles/styles';
+// import { submitContract } from '../store/actions/'
 import Dropzone from 'react-dropzone'
 import Header from '../components/Header';
-
-export default class ContractForm extends Component {
+class ContractForm extends Component {
+    
     render() {
         return (
             <Container>
@@ -12,18 +15,23 @@ export default class ContractForm extends Component {
                     <h1> Contracts </h1>
                     <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
                         {({ getRootProps, getInputProps }) => (
-                            <section>
+                            <section className="ContractDropZone">
                                 <div {...getRootProps()}>
-                                    <input {...getInputProps()} />
-                                    <p>drop the contract here, or click to select it</p>
+                                    <input  {...getInputProps()} />
+                                    <p>
+                                        drop the contract here, or click to select it
+                                    </p>
                                 </div>
                             </section>
                         )}
                     </Dropzone>
                 </div>
-
             </Container>
-
         );
     }
 }
+
+const mapDispatchToProps = dispatch =>
+    bindActionCreators({ submitForm }, dispatch);
+
+export default connect(mapDispatchToProps)(ContractForm);
--- src/styles/styles.js
+++ src/styles/styles.js
@@ -50,5 +50,8 @@ transition: transform .5s ease;
         box-shadow: 1px 1px 0.8px lightgray;
     }
 }
-
+.ContractDropZone {
+    width: 100%;
+    height: 100%;
+}
 `;
