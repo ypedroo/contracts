fe556fd9e15a201dec2203e7f8eca11b76c91ab4
ypedro <ynoa.pedro@outlook.com>
2020-01-30 20:49:05 -0300

    Change folder structure for contract page

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/pages/Index';
 import PartForm from './components/pages/PartForm/PartForm';
-import ContractForm from './components/pages/ContractForm';
+import ContractForm from './components/pages/ContractForm/ContractForm';
 import { Container } from './styles/styles';
 
 function App() {
